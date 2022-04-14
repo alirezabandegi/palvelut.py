42702861d30f995d75bc867e41e05cf2faf88890
Alireza Bandegi <60815121+alirezabandegi@users.noreply.github.com>
2022-04-14 22:59:33 +0300

    __luo_nro added

--- palvelut.py
+++ palvelut.py
@@ -7,3 +7,12 @@ class Asiakas:
     self.__nimi = nimi
     self.__asiakasnumero = self.__luo_nro
     self.__ika = ika
+  
+  def __luo_nro(self):
+    number = []
+
+    number.append(random.randint(0, 99))
+    number.append(random.randint(0, 999))
+    number.append(random.randint(0, 999))
+
+    return number
