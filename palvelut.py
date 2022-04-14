17bab4e3ba2d0e51e39f1afe48cf3d23d5ea2eee
Alireza Bandegi <60815121+alirezabandegi@users.noreply.github.com>
2022-04-15 01:00:35 +0300

    palvelu class docstrings added

--- palvelut.py
+++ palvelut.py
@@ -61,31 +61,56 @@ class Asiakas:
     sijoittaa asikkaan numerot ja palauttaa niitä
     """
     return f'{Asiakas.__asiakasnumero[0]:02}-{Asiakas.__asiakasnumero[1]:03}-{Asiakas.__asiakasnumero[2]:03}'
-  
+
 class Palvelu(Asiakas):
-   
+  """Luokka, joka tulostaa asikkaan tiedot.
+
+    :ivar tuotenimi: tuotenimi, arvotaan
+    :type tuotenimi: str
+    :ivar __asiakkaat: asiakkaiden nimet, ottaa kaikki asiakkaiden nimet.
+    :type __asiakkaat: list
+
+    Julkiset metodit:
+      lisaa_asiakas()
+      poista_asiakas()
+      tulosta_asiakkaat()
+      
+    suojatut metodit:
+      _luo_asiakarivi(str & list(int))
+  """    
   def __init__(self, tuotenimi):
     """Konstruktori"""
     self.tuotenimi = tuotenimi
     self.__asiakkaat = []
 
   def _luo_asiakasrivi(self, Asiakas):
+    """rakentaa asikas tiedot 
+      :return: asikas tiedot
+      :rtype: str
+    """
+
     return f"{self.set_nimi} ({self.set_numero}) on {self.set_ika}-vuotias."
  
   def lisaa_asiakas(self, Asiakas):
+    """lisää asikas __asikkaat listaan"""
+    
     try:
       self.__asiakkaat.append(Asiakas)
     except ValueError:
       pass
   
   def poista_asiakas(self, Asiakas):
+    """poitaa asikas __asikkaat listasta"""
     try:
       self.__asiakkaat.pop(Asiakas)
     except ValueError:
         pass
 
   def tulosta_asiakkaat(self):
+    """
+    tulostaa asikas tiedot.
+    """
+
     print(f" tuotteen " + self.tuotenimi + " asiakkaat ovat: ")
     for Asiakas in self.__asiakkaat:
       print(self._luo_asiakasrivi(Asiakas))
-  
